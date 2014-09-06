ccdf55fdf60c32a9f9741792e185f747f0cd8e63
Charanjit Singh <ckhabra007@gmail.com>
2014-09-06 14:13:14 +0530

    Upgraded to meteor 0.9.1

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
-    summary: "MVC like models in meteor"
+  summary: "MVC like models in meteor",
+  version: "0.0.2",
+  git: "https://github.com/channikhabra/meteor-stupid-models.git"
 });
 
 Package.on_use(function (api, where) {
-    api.use(["underscore"]);
+  api.versionsFrom("METEOR@0.9.1");
+  api.use(["underscore"]);
 
-    api.add_files([
-        'lib/base_model.js',
-        'lib/model_factory.js',
-    ], ['client', 'server']);
-    api.export && api.export(['Model'], ['client', 'server']);
+  api.add_files([
+    'lib/base_model.js',
+    'lib/model_factory.js',
+  ], ['client', 'server']);
+  api.export && api.export(['Model'], ['client', 'server']);
 });
