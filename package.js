67e8f5893bdc672de7ec4214cfe4c618d16527fd
James Gillmore <james@faceyspacey.com>
2015-01-10 21:55:40 -0800

    all files added

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "MVC-like models in meteor",
+  version: "0.0.6",
+  git: "https://github.com/nucleuside/meteor-smart-models.git"
+});
+
+Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.1");
+  api.use(["underscore"]);
+
+  api.add_files([
+    'lib/base_model.js',
+    'lib/model_factory.js',
+  ], ['client', 'server']);
+  api.export && api.export(['Model'], ['client', 'server']);
+});
