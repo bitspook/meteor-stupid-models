5ad592b325838e75bf511730d23459ca862232a9
James Gillmore <james@faceyspacey.com>
2015-01-10 22:34:27 -0800

    change version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "MVC-like models in meteor",
-  version: "0.0.6",
+  version: "0.0.7",
   git: "https://github.com/nucleuside/meteor-smart-models.git",
   name: "nucleuside:smart-models"
 });
